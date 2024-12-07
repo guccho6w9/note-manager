a49329f2bffdac741f57d092e5e9142128b6d95b
guccho6w9 <gusta.quiroga033@gmail.com>
2024-12-07 18:03:29 -0300

    Add(-): updating dependencies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();  // Cargar las variables del archivo .env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   // Configuración de CORS
   app.enableCors({
     origin: 'http://localhost:5173', // Permite solicitudes desde el frontend
